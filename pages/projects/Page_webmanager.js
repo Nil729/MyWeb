8619674fee180a17eb8c6bbf45e2c5a74bd43ea1
nil pinyana <nil.pinyana@gmail.com>
2022-08-29 17:57:01 +0200

    compWebmanager

--- pages/projects/Page_webmanager.js
+++ pages/projects/Page_webmanager.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/Home.module.css';
 import Navbar from '../../Components/Navbar/Navbar';
-import Webmanager from '../../Components/Webmanager/Webmanager';
+import {Webmanager} from '../../Components/CompWebmanager/Webmanager';
 
 export default function Page_render_webmanager() {
     return (
