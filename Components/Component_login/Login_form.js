634bcd525c116c8e1d63b2ac6b2f5b96b88863b2
nil pinyana <nil.pinyana@gmail.com>
2022-09-19 12:42:39 +0200

    baisic login and singup

---
+++ Components/Component_login/Login_form.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export default function LoginForm(){
+
+    return (
+        <> 
+            <div>
+                <input type="text"></input>
+                <input type="text"></input>
+                <input type="submit"></input>
+            </div>
+        </>
+    );
+};
---
+++ Components/Component_signup/Singup_form.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export default function SingupForm(){
+
+    return (
+        <> 
+            <div>
+                <input type="text"></input>
+                <input type="text"></input>
+                <input type="text"></input>
+                <input type="text"></input>
+                <input type="submit"></input>
+            </div>
+        </>
+    );
+};
