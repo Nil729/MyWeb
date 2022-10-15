2bbcea628b0dddc92fa1969a363a23e566753683
nil pinyana <nil.pinyana@gmail.com>
2022-10-15 18:14:08 +0200

    Login_form

--- Components/Component_login/Login_form.js
+++ Components/Component_login/Login_form.js
@@ -2,16 +2,33 @@ import { useState } from "react";
 
 export default function LoginForm(){
 
-    const postLoginForm = () =>{
-        
-    }
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    });
 
+    const handlechanges = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value, // e.target.value = nil e.target.name = user_name (name input)
+        })
+    };
+    const handleSubmit = (e) =>{
+        e.preventDefault();  // Catch a default e value
+        console.log(credentials)
+    }
     return (
         <> 
             <div>
-                <input id="useName" type="text"></input>
-                <input id="userPsw" type="text"></input>
-                <input type="submit"></input>
+                <form onSubmit={handleSubmit}>
+                    <input name="username" id="useName" type="text"
+                        onChange={handlechanges}
+                    /> 
+                    <input name="password" id="userPsw" type="password"
+                        onChange={handlechanges}
+                    />
+                    <button>login</button>
+                </form>
             </div>
         </>
     );
--- server/controller/session.controllers.js
+++ server/controller/session.controllers.js
@@ -2,25 +2,25 @@ import con from "../config/db.config.js"
 
 
 export const login = async () => {
-    let userForm = req.body;
-    let dataUserFormLogin= [userForm.username, userForm.userpsw];
-    try {
-        const sqlPostDataUserLogin = 'INSERT INTO `db_web`.`users` (`user_name`, `user_password`, `user_id`) VALUES (?, ?, ?)';
-        con.query(sqlPostDataUserLogin, dataUserFormLogin, (err, result)=> {
-          if (err) throw err;
-        });
-    } catch (err) {console.log('Error: ', err.message);};
+  let userForm = req.body;
+  let dataUserFormLogin= [userForm.username, userForm.userpsw];
+  try {
+      const sqlPostDataUserLogin = 'INSERT INTO `db_web`.`users` (`user_name`, `user_password`, `user_id`) VALUES (?, ?, ?)';
+      con.query(sqlPostDataUserLogin, dataUserFormLogin, (err, result)=> {
+        if (err) throw err;
+      });
+  } catch (err) {console.log('Error: ', err.message);};
 };
 
 
 export const singup = async (req,res) => {
   let userSingupForm = req.body;
-  let dataUserForm= [userSingupForm.userName, userSingupForm.userFullName, userSingupForm.userEmail, userSingupForm.userPSW];
+  const dataUserForm= [userSingupForm.userName, userSingupForm.userFullName, userSingupForm.userEmail, userSingupForm.userPSW];
   try {
       const sqlPostDataUserSingup = 'INSERT INTO `db_web`.`users` (`user_name`, `full_name`, `email`, `user_psw`) VALUES (?, ?, ?, ?)';
       con.query(sqlPostDataUserSingup, dataUserForm, (err, result)=> {
         if (err) throw err;
       });
-  } catch (err) {console.log('Error: ', err.message);};
 
+  } catch (err) {console.log('Error: ', err.message);};
 };
